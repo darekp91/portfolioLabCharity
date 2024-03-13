ee9d927bf92efe9b63ccef5c58669012660ecb9f
darekp91 <darekp91@gmail.com>
2024-03-13 17:29:57 +0100

    institution view on the website

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -2,15 +2,37 @@ package pl.coderslab.charity;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.service.DonationService;
+import pl.coderslab.charity.service.InstitutionService;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    private final InstitutionService institutionService;
+    private final DonationService donationService;
 
-    @RequestMapping("/")
-    public String homeAction(Model model){
-        return "index";
+    public HomeController(InstitutionService institutionService, DonationService donationService) {
+        this.institutionService = institutionService;
+        this.donationService = donationService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // Obsługa logiki potrzebnej na stronie głównej
+        // Pobranie instytucji
+        List<Institution> institutions = institutionService.getAllInstitutions();
+        model.addAttribute("institutions", institutions);
+        // Obliczenie sumy worków
+        int totalBags = donationService.getTotalBags();
+        model.addAttribute("totalBags", totalBags);
+        // ...
+        return "index"; // lub nazwa widoku, który chcesz zwrócić
     }
 }
+
